47714936e8d67dbdefd6b83797d72324ecb033fd
jath03 <jackthughes@yahoo.com>
2023-03-22 23:29:46 -0700

    Added scanning and displaying devices

--- src/screens/ConnectDeviceScreen/ConnectDeviceScreen.js
+++ src/screens/ConnectDeviceScreen/ConnectDeviceScreen.js
@@ -1,10 +1,167 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator, Text, View, FlatList, SafeAreaView, Platform, PermissionsAndroid, TouchableOpacity, NativeModules, NativeEventEmitter } from 'react-native'
+import styles from './styles';
+import BleManager, {
+  BleDisconnectPeripheralEvent,
+  BleManagerDidUpdateValueForCharacteristicEvent,
+  BleScanCallbackType,
+  BleScanMatchMode,
+  BleScanMode,
+  Peripheral,
+  ScanSettings
+} from 'react-native-ble-manager';
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 export default function ConnectDeviceScreen(props) {
+    const [isScanning, setIsScanning] = useState(false);
+    const [peripherals, setPeripherals] = useState(new Map());
+
+    const addOrUpdatePeripheral = (id: string, updatedPeripheral: Peripheral) => {
+        setPeripherals(map => new Map(map.set(id, updatedPeripheral)));
+    };
+
+    useEffect(() => {
+        try {
+        BleManager.start({showAlert: false})
+            .then(() => console.debug('BleManager started.'))
+            .catch(error =>
+            console.error('BleManager could not be started.', error),
+            );
+        } catch (error) {
+        console.error('unexpected error starting BleManager.', error);
+        return;
+        }
+
+        const listeners = [
+            bleManagerEmitter.addListener(
+                'BleManagerDiscoverPeripheral',
+                handleDiscoverPeripheral,
+            ),
+            bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan),
+        // bleManagerEmitter.addListener(
+        //     'BleManagerDisconnectPeripheral',
+        //     handleDisconnectedPeripheral,
+        // ),
+        // bleManagerEmitter.addListener(
+        //     'BleManagerDidUpdateValueForCharacteristic',
+        //     handleUpdateValueForCharacteristic,
+        // ),
+        ];
+
+        handleAndroidPermissions();
+
+        return () => {
+            console.debug('[app] main component unmounting. Removing listeners...');
+            for (const listener of listeners) {
+                listener.remove();
+            }
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const startScan = () => {
+        if (!isScanning) {
+            // reset found peripherals before scan
+            setPeripherals(new Map());
+
+            try {
+                console.debug('[startScan] starting scan...');
+                setIsScanning(true);
+                // BleManager.scan(["6E400001-B5A3-F393-E0A9-E50E24DCCA9E"], 5, false).then(() => {  // Nordic UART service UUID
+                BleManager.scan([], 5, false).then(() => {
+                    console.debug('[startScan] scan promise returned successfully.');
+                }).catch(err => {
+                    console.error('[startScan] ble scan returned in error', err);
+                });
+            } catch (error) {
+                console.error('[startScan] ble scan error thrown', error);
+            }
+        }
+    };
+
+    const handleStopScan = () => {
+        setIsScanning(false);
+        console.debug('[handleStopScan] scan is stopped.');
+        console.debug(peripherals);
+        console.debug(peripherals.length);
+    };
+
+    const handleDiscoverPeripheral = (peripheral: Peripheral) => {
+        console.debug('[handleDiscoverPeripheral] new BLE peripheral=', peripheral);
+        if (peripheral.name) {
+        // peripheral.name = 'NO NAME';
+            addOrUpdatePeripheral(peripheral.id, peripheral);
+        }
+    };
+
+    const handleAndroidPermissions = () => {
+        if (Platform.OS === 'android' && Platform.Version >= 31) {
+        PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        ]).then(result => {
+            if (result) {
+            console.debug(
+                '[handleAndroidPermissions] User accepts runtime permissions android 12+',
+            );
+            } else {
+            console.error(
+                '[handleAndroidPermissions] User refuses runtime permissions android 12+',
+            );
+            }
+        });
+        } else if (Platform.OS === 'android' && Platform.Version >= 23) {
+        PermissionsAndroid.check(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        ).then(checkResult => {
+            if (checkResult) {
+            console.debug(
+                '[handleAndroidPermissions] runtime permission Android <12 already OK',
+            );
+            } else {
+            PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            ).then(requestResult => {
+                if (requestResult) {
+                console.debug(
+                    '[handleAndroidPermissions] User accepts runtime permission android <12',
+                );
+                } else {
+                console.error(
+                    '[handleAndroidPermissions] User refuses runtime permission android <12',
+                );
+                }
+            });
+            }
+        });
+        }
+    };
+
+
+    const Separator = () => <View style={styles.separator} />;
+
+    const PeripheralItem = ({item}) => (
+        <View style={styles.device}>
+            <Text style={styles.buttonTitle}>{item.name}</Text>
+        </View>
+    );
+
     return (
-        <View>
-            <Text>Connect Your Device</Text>
+        <View style={styles.container}>
+            <Separator />
+            <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity style={styles.button} onPress={startScan}>
+                <Text style={styles.buttonTitle}>Scan for Devices</Text>
+            </TouchableOpacity>
+
+            <ActivityIndicator animating={isScanning} />
+            </View>
+            <Separator />
+                <FlatList
+                    data={Array.from(peripherals.values())}
+                    renderItem={PeripheralItem}
+                />
         </View>
     )
 }
