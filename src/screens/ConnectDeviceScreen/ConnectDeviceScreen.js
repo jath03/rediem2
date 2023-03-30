49d6e543c8195a11c80c241fcb485644fa3fb9d3
jath03 <jackthughes@yahoo.com>
2023-03-29 21:13:57 -0700

    Got data transfer with rediem format working

--- src/screens/ConnectDeviceScreen/ConnectDeviceScreen.js
+++ src/screens/ConnectDeviceScreen/ConnectDeviceScreen.js
@@ -43,10 +43,10 @@ export default function ConnectDeviceScreen(props) {
         //     'BleManagerDisconnectPeripheral',
         //     handleDisconnectedPeripheral,
         // ),
-        // bleManagerEmitter.addListener(
-        //     'BleManagerDidUpdateValueForCharacteristic',
-        //     handleUpdateValueForCharacteristic,
-        // ),
+            bleManagerEmitter.addListener(
+                'BleManagerDidUpdateValueForCharacteristic',
+                handleUpdateValueForCharacteristic,
+            ),
         ];
 
         handleAndroidPermissions();
@@ -68,8 +68,7 @@ export default function ConnectDeviceScreen(props) {
             try {
                 console.debug('[startScan] starting scan...');
                 setIsScanning(true);
-                // BleManager.scan(["6E400001-B5A3-F393-E0A9-E50E24DCCA9E"], 5, false).then(() => {  // Nordic UART service UUID
-                BleManager.scan([], 5, false).then(() => {
+                BleManager.scan(["6E400001-B5A3-F393-E0A9-E50E24DCCA9E"], 5, false).then(() => {  // Nordic UART service UUID
                     console.debug('[startScan] scan promise returned successfully.');
                 }).catch(err => {
                     console.error('[startScan] ble scan returned in error', err);
@@ -86,14 +85,122 @@ export default function ConnectDeviceScreen(props) {
         console.debug(peripherals);
         console.debug(peripherals.length);
     };
+    
+    let buffer = new ArrayBuffer(24);
+    // Writing to the buffer using uint8's because that's what we receive from
+    // data.value below in handleUpdateValueForCharacteristic
+    let uint8_view = new Uint8Array(buffer);
+    
+    // Reading from the buffer, we want to see the int16 and float values that 
+    // we sent from the arduino
+    let int16_view = new Int16Array(buffer, 0, 6);
+    let float_view = new Float32Array(buffer, 12, 3);
+    
+    let buffer_idx = 0;
+    
+    const handleUpdateValueForCharacteristic = (
+        data: BleManagerDidUpdateValueForCharacteristicEvent,
+    ) => {
+        // The data doesn't necessarily come all in one packet, so we have to
+        // manage this buffer filling up ourselves
+        if (buffer_idx == 0 && data.value[0] == 0x12 && data.value[1] == 0x34) {
+            for (let i = 0; i < data.value.length - 2; i++) {
+                uint8_view[i] = data.value[i + 2];
+            }
+            buffer_idx += data.value.length - 2;
+        } else if (buffer_idx > 0) {
+            for (let i = 0; i < data.value.length; i++) {
+                uint8_view[buffer_idx + i] = data.value[i];
+            }
+            if (buffer_idx + data.value.length >= 24) {
+                console.log("Revieved data: " + int16_view + "," + float_view);
+                buffer_idx = 0;
+            } else {
+                buffer_idx += data.value.length;
+            }
+        }
+    };
 
     const handleDiscoverPeripheral = (peripheral: Peripheral) => {
         console.debug('[handleDiscoverPeripheral] new BLE peripheral=', peripheral);
         if (peripheral.name) {
         // peripheral.name = 'NO NAME';
+            peripheral.connected = false;
+            peripheral.connecting = false;
             addOrUpdatePeripheral(peripheral.id, peripheral);
         }
     };
+    
+    const connectPeripheral = async (peripheral: Peripheral) => {
+        try {
+            if (peripheral) {
+                addOrUpdatePeripheral(peripheral.id, {...peripheral, connecting: true});
+        
+                await BleManager.connect(peripheral.id);
+                console.debug(`[connectPeripheral][${peripheral.id}] connected.`);
+        
+                addOrUpdatePeripheral(peripheral.id, {
+                ...peripheral,
+                connecting: false,
+                connected: true,
+                });
+        
+                // before retrieving services, it is often a good idea to let bonding & connection finish properly
+                await sleep(900);
+        
+                /* Test read current RSSI value, retrieve services first */
+                const peripheralData = await BleManager.retrieveServices(peripheral.id);
+                console.debug(
+                `[connectPeripheral][${peripheral.id}] retrieved peripheral services`,
+                peripheralData,
+                );
+        
+                const rssi = await BleManager.readRSSI(peripheral.id);
+                console.debug(
+                `[connectPeripheral][${peripheral.id}] retrieved current RSSI value: ${rssi}.`,
+                );
+        
+                if (peripheralData.characteristics) {
+                    for (let characteristic of peripheralData.characteristics) {
+                        if (characteristic.descriptors) {
+                            for (let descriptor of characteristic.descriptors) {
+                                try {
+                                    let data = await BleManager.readDescriptor(peripheral.id, characteristic.service, characteristic.characteristic, descriptor.uuid);
+                                    console.debug(`[connectPeripheral][${peripheral.id}] descriptor read as:`, data);
+                                } catch (error) {
+                                    console.error(`[connectPeripheral][${peripheral.id}] failed to retrieve descriptor ${descriptor} for characteristic ${characteristic}:`, error);
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                BleManager.startNotification(
+                    peripheral.id,
+                    "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
+                    "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+                ).then(() => {
+                    console.debug("[connectPeripheral] Enabled notification");
+                }).catch((error) => {
+                    console.debug(error);
+                });
+    
+                let p = peripherals.get(peripheral.id);
+                if (p) {
+                    addOrUpdatePeripheral(peripheral.id, {...peripheral, rssi});
+                }
+            }
+        } catch (error) {
+        console.error(
+            `[connectPeripheral][${peripheral.id}] connectPeripheral error`,
+            error,
+        );
+        }
+    };
+    
+    function sleep(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     const handleAndroidPermissions = () => {
         if (Platform.OS === 'android' && Platform.Version >= 31) {
@@ -142,9 +249,12 @@ export default function ConnectDeviceScreen(props) {
     const Separator = () => <View style={styles.separator} />;
 
     const PeripheralItem = ({item}) => (
-        <View style={styles.device}>
-            <Text style={styles.buttonTitle}>{item.name}</Text>
-        </View>
+        <TouchableOpacity style={styles.device} onPress={() => connectPeripheral(item)}>
+            <Text style={styles.buttonTitle}>
+                {item.name}
+                {item.connecting && ' - Connecting...'}
+            </Text>
+        </TouchableOpacity>
     );
 
     return (
