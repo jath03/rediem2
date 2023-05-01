8fc23d351ebd967050449f0d6f3ba2dc368959a8
jath03 <jackthughes@yahoo.com>
2023-05-01 00:32:54 -0700

    WIP charts

--- src/screens/RecordDataScreen/RecordDataScreen.js
+++ src/screens/RecordDataScreen/RecordDataScreen.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Text, View, TouchableOpacity, NativeModules, NativeEventEmitter } from 'react-native'
-import { jsonToCSV } from 'react-native-csv';
-import Share from 'react-native-share';
-import styles from './styles';
+import { Text, View, SafeAreaView, TouchableOpacity, NativeModules, NativeEventEmitter } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import { jsonToCSV } from 'react-native-csv'
+import Share from 'react-native-share'
+import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-responsive-linechart'
+import styles from './styles'
 import BleManager, {
   BleDisconnectPeripheralEvent,
   BleManagerDidUpdateValueForCharacteristicEvent,
@@ -11,14 +13,18 @@ import BleManager, {
   BleScanMode,
   Peripheral,
   ScanSettings
-} from 'react-native-ble-manager';
+} from 'react-native-ble-manager'
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+
+const FIELDS = ["R1", "R2", "R3", "R4", "R5", "R6", "X", "Y", "Z", "EMG", "BIOZ"];
+// const FIELD_NAMES = [""]
 
 export default function RecordDataScreen(props) {
     const [currentData, setCurrentData] = useState(new Array());
     const [recording, setRecording] = useState(false);
-    let recordedData = useRef([]);
+    const [recordedData, setRecordedData] = useState([]);
+    const startTime = useRef(Date.now())
 
     useEffect(() => {
         const listeners = [
@@ -40,17 +46,18 @@ export default function RecordDataScreen(props) {
     const toggleRecording = () => {
         if (!recording) {
             console.debug("Starting recording");
-            recordedData.current = [];
+            setRecordedData([]);
+            startTime.current = Date.now();
         } else {
             console.debug("Stopping recording");
-            console.debug("# of entries recorded: " + recordedData.current.length);
+            console.debug("# of entries recorded: " + recordedData.length);
         }
         setRecording(recording => !recording);
     };
 
     const exportData = async () => {
         let data_str = jsonToCSV({
-            fields: ["R1", "R2", "R3", "R4", "R5", "R6", "X", "Y", "Z", "EMG", "BIOZ"],
+            fields: FIELDS,
             data: recordedData.current
         });
         const prefix = 'data:text/csv;base64,';
@@ -87,6 +94,7 @@ export default function RecordDataScreen(props) {
     ) => {
         // The data doesn't necessarily come all in one packet, so we have to
         // manage this buffer filling up ourselves
+        console.debug(data.value);
         switch (buffer_state) {
             case 0:
                 if (data.value[0] != 0x12) {
@@ -114,13 +122,16 @@ export default function RecordDataScreen(props) {
                     uint8_view[buffer_idx + i - offset] = data.value[i];
                 }
                 if (buffer_idx + data.value.length - offset >= 32) {
-                    tmp_data = Array.from(int16_view).concat(Array.from(float_view));
-                    for (let i = 0; i < tmp_data.length; i++) {
-                        tmp_data[i] = tmp_data[i].toFixed(2);
-                    }
+                    tmp_data = [Date.now(), ...Array.from(int16_view).concat(Array.from(float_view))];
+                    // for (let i = 1; i < tmp_data.length; i++) {
+                    //     tmp_data[i] = tmp_data[i].toFixed(2);
+                    // }
                     setCurrentData(tmp_data);
                     if (recording) {
-                        recordedData.current.push(tmp_data);
+                        setRecordedData(recdata => {
+                            recdata.push(tmp_data);
+                            return recdata;
+                        });
                     }
                     console.log("Received data: " + tmp_data);
                     buffer_idx = 0;
@@ -132,6 +143,56 @@ export default function RecordDataScreen(props) {
         }
     };
 
+    const transpose = (matrix) => {
+        let transposed = matrix[0].map((col, i) => matrix.map(row => row[i]));
+        let timestamps = transposed.shift();
+        let out = transposed.map(item => item.map((e, i) => {
+            // console.debug(e);
+            // console.debug(timestamps);
+            return {
+                // date: new Date(timestamps[i] - startTime.current),
+                // value: e
+                x: (timestamps[i] - startTime.current) / 1000,
+                y: e
+            };
+        }));
+        // console.debug(out);
+        return out;
+    };
+
+    const GraphItem = ({item, index}) => {
+        // console.debug("Graphing item " + item);
+        // console.debug(item);
+        return (
+            <View>
+                <Text style={{color: '#000000'}}>{FIELDS[index]}</Text>
+                <Chart
+                    disableGestures
+                    data={item}
+                    style={{height: 200, width: 300}}
+                    yDomain={{ min: 0, max: 10}}
+                >
+                    <VerticalAxis />
+                    <HorizontalAxis />
+                    <Line />
+                    <Area />
+                </Chart>
+            </View>
+        );
+        // return (
+        //   <Text>{item.toString()}</Text>  
+        // );
+    };
+
+    // const graphs = (data) => {
+    //     let graphItems = [];
+    //     data = transpose(data);
+    //     for (variable in data) {
+    //         graphItems.push(
+    //             <FlatList
+    //         );
+    //     }
+    // };
 
     return (
         <View style={styles.container}>
@@ -156,9 +217,20 @@ export default function RecordDataScreen(props) {
                     Live Data:
                 </Text>
                 <Text style={{color: '#000000'}}>
-                    {currentData.toString()}
+                    {currentData.slice(1).map(i => i.toFixed(2)).toString()}
                 </Text>
             </View>
+            <SafeAreaView style={{alignItems: 'center'}}>
+                {recordedData.length > 1 ?
+                    <FlatList
+                        data={transpose(recordedData)}
+                        renderItem={GraphItem}
+                        contentContainerStyle={{ paddingBottom: 200 }}
+                    />
+                    :
+                    <Text>Hit record for visualization</Text>
+                }
+            </SafeAreaView>
         </View>
     )
 }
